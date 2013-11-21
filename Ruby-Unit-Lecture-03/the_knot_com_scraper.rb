819f2e86c86104e2c85a5acad0cd943e5eb332eb
Sze Chan <schan@xogrp.com>
2013-11-21 13:18:38 -0500

    commit/pull/merge

--- Ruby-Unit-Lecture-03/the_knot_com_scraper.rb
+++ Ruby-Unit-Lecture-03/the_knot_com_scraper.rb
@@ -1,21 +1,22 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open('http://localhost:3000')
+html = open('http://localhost:3000/alaska.html')
+html = open('http://localhost:3000/alabama01.html')
 
 doc = Nokogiri::HTML(html)
 
-links = doc.css("a")
+#links = doc.css(".vendorSummaryInfo a")
 
-links.each do |link|
-	puts link.attributes["href"].value
-end
+#links.each do |link|
+#	puts link.attributes["href"].value
+#end
 
 
 def node_text(nokogiri,selector)
 	values = nokogiri.css(selector)
 	values.each do |value|
-		puts value.content
+		puts value.content.strip
 	end
 end
 
@@ -25,8 +26,15 @@ def puts_section_label(text)
 	puts "-"*30
 end
 
-puts_section_label("<h2> node text")
-node_text(doc,"h2")
+#puts_section_label("<h2> node text")
+#node_text(doc,"h2")
 
-puts_section_label("<a> node text")
-node_text(doc,"a")
+#puts_section_label("<a> node text")
+#node_text(doc,"a")
+
+puts_section_label("<a> vendorSummaryInfo")
+node_text(doc,".vendorSummaryInfo a")
+node_text(doc,".vendorSummaryInfo div")
+
+
+#http://www.theknot.com/Vendors/New-Hampshire-Wedding-Venues/Profiles/CAR/202?page=1.ALL
